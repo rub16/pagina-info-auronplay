fcd8249580f709d39cb44b31577b9180681b3322
rub16 <167896318+rub16@users.noreply.github.com>
2024-04-23 10:49:28 -0600

    Add files via upload

---
+++ Pwruby/Pruby.js
@@ -0,0 +1,9 @@
+var prg= prompt("Te gustaria saber un poco sobre auronplay?");
+switch(prg){
+    case "si":
+    alert("a continuacion te mostrare un poco de informacion sobre auronplay")
+     break;
+    case "no":
+    alert("okey, pero aqui tines informacion sobre el :)");
+        break;
+}
